9170cd17c4d144882f674c090dffa09394ad0944
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-16 11:25:12 -0500

    Refactor chat.js code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,68 +1,69 @@
 
-const username = localStorage.getItem('name');
+const username = localStorage.getItem( 'name' );
 if ( !username ) {
-  window.location.replace('/')
-  throw new Error('Username is required');
+  window.location.replace( '/' );
+  throw new Error( 'Username is required' );
 }
 
-const socket = io({
+const socket = io( {
+  auth: {
+    token: 'ABC-123',
+    name: username
+  },
   extraHeaders: {
     'user-name': username,
   }
-});
+} );
 
 
-const usersUlElement = document.querySelector('ul');
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const chatElement = document.querySelector('#chat');
+const usersUlElement = document.querySelector( 'ul' );
+const form = document.querySelector( 'form' );
+const input = document.querySelector( 'input' );
+const chatElement = document.querySelector( '#chat' );
 
 
-const renderUsers = (users) => {
+const renderUsers = ( users ) => {
 
   usersUlElement.innerHTML = '';
-  users.forEach((user) => {
-    const liElement = document.createElement('li');
+  users.forEach( ( user ) => {
+    const liElement = document.createElement( 'li' );
     liElement.innerText = user.name;
-    usersUlElement.appendChild(liElement);
-  });
-
-  // Scroll to bottom
-  setTimeout(() => {
-    chatElement.scrollTop = chatElement.scrollHeight;
-  }, 100);
+    usersUlElement.appendChild( liElement );
+  } );
 
 };
 
-const renderMessage = (payload) => {
-  const { userId, message, userName } = payload
-  
-  const divElement = document.createElement('div');
-  divElement.classList.add('message');
+const renderMessage = ( payload ) => {
+  const { userId, message, userName } = payload;
+
+  const divElement = document.createElement( 'div' );
+  divElement.classList.add( 'message' );
 
   if ( userId !== socket.id ) {
-    divElement.classList.add('incoming');
+    divElement.classList.add( 'incoming' );
   }
 
   divElement.innerHTML = message;
-  chatElement.appendChild(divElement);
+  chatElement.appendChild( divElement );
 
   // Scroll al final de los mensajes
   chatElement.scrollTop = chatElement.scrollHeight;
-}
 
 
-socket.on('on-clients-changed', renderUsers );
-socket.on('on-message', renderMessage );
+};
+
+
+socket.on( 'on-clients-changed', renderUsers );
+socket.on( 'on-message', renderMessage );
 
 
 
-form.addEventListener('submit', (event) => {
+form.addEventListener( 'submit', ( event ) => {
   event.preventDefault();
-  
+
   const message = input.value;
   input.value = '';
 
-  socket.emit('send-message', message);
+  socket.emit( 'send-message', message );
 
-});
+} );
